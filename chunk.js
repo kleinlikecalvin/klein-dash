f47cf5be8f54a8eb6c64441f5946d29854bd4d00
streetlightkids <ltj_hq@hotmail.com>
2023-03-29 17:37:28 -0700

    chunk.js
    My solution for how Lodash _.chunk() works

---
+++ chunk.js
@@ -0,0 +1,30 @@
+/*
+ * @params {Array<any>} array
+ * @params {number} [size=1] - length of chunk(s)
+ * @returns {Array<any>}
+ */
+let ogArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+chunk(ogArray, 3);
+
+function chunk(array, size) {
+  if (size <= 1) {
+    return array;
+  }
+
+  let newArray = [];
+  let chunk = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (chunk.length === size) {
+      newArray.push(chunk);
+      chunk = [];
+    }
+    chunk.push(array[i]);
+  }
+
+  if (chunk.length) {
+    newArray.push(chunk);
+  }
+
+  return newArray;
+}
